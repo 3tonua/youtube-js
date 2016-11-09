63d2a0fce4ff4603ed315cd5e08c774cb64e0440
Ivanov Anton <3ton.ua@gmail.com>
2016-11-09 20:53:11 +0200

    1st

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ $.when(YouTube.getLastVideos()).then(function (result) {
 function renderVideos(videos) {
     videos.forEach(function (data, index) {
         var video = '<div class="col-sm-6 col-md-4">' +
-            '<div class="thumbnail">' +
-            '<img src=" ' + data.snippet.thumbnails.high.url + ' " alt="...">' +
-            '<div class="caption">' +
-            '<h3>' + data.snippet.title + '</h3>' +
-            '<p>' + data.snippet.channelTitle + ' / ' + data.snippet.publishedAt + '</p>' +
-            '<p>' + data.snippet.description + '</p>' +
-            '<p><a href="#" class="btn btn-primary" role="button">Button</a>' +
-            '<a href="#" class="btn btn-default" role="button">Button</a></p>' +
-            '</div>' +
-            '</div>' +
+                        '<div class="thumbnail">' +
+                            '<img src=" ' + data.snippet.thumbnails.high.url + ' " alt="...">' +
+                                '<div class="caption">' +
+                                    '<h3>' + data.snippet.title + '</h3>' +
+                                    '<p>' + data.snippet.channelTitle + ' / ' + data.snippet.publishedAt + '</p>' +
+                                    '<p>' + data.snippet.description + '</p>' +
+                                    '<p><a href="#" class="btn btn-primary" role="button">Button</a>' +
+                                    '<a href="#" class="btn btn-default" role="button">Button</a></p>' +
+                            '</div>' +
+                        '</div>' +
             '</div>';
         $('#last .row').append(video);
     })
